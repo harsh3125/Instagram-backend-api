4e7103550c8c9c8940b43c113bae786d51e56307
harsh3125 <55591116+harsh3125@users.noreply.github.com>
2021-10-09 01:13:31 +0530

    Create testcase.go

---
+++ testcase/testcase.go
@@ -0,0 +1,24 @@
+ackage main
+
+import (
+	"fmt"
+	"github.com/rishabhjohri/Instagram-backend-api"
+	"os"
+)
+
+func main() {
+
+	client := instagram.NewClient(nil)
+	client.ClientID = "8f2c0ad697ea4094beb2b1753b7cde9c"
+
+	media, next, err := client.Media.Popular()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	}
+	for _, m := range media {
+		fmt.Printf("ID: %v, Type: %v\n", m.ID, m.Type)
+	}
+	if next.NextURL != "" {
+		fmt.Println("Next URL", next.NextURL)
+	}
+}
